4a0bd8560eccb57b81b676063bcb80dab6f13288
liaohuanxuan.zzq <liaohuanxuan.zzq@bytedance.com>
2025-06-07 19:36:31 +0800

    max
    Signed-off-by: liaohuanxuan.zzq <liaohuanxuan.zzq@bytedance.com>

--- scripts/scripts_longBench/ee.sh
+++ scripts/scripts_longBench/ee.sh
@@ -4,11 +4,19 @@ method=$2 # Support PyramidKV, SnapKV, H2O, StreamingLLM, CAM, L2Norm, ThinK
 pruning_ratio=${3:-"0.4"} # Support 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9
 max_capacity_prompts=${4:-"512"} # 128,2048 in paper
 attn_implementation=eager # Support "flash_attention_2", "sdpa", "eager".
-source_path=results/
-model_path="meta-llama/Meta-Llama-3-8B-Instruct"
 
 input_length=${5:-"14000"} # Support 128, 512, 1024, 2048, 4096, 8192, 16384
 output_length=${6:-"338"} # Support 128, 512, 1024, 2048, 4096, 8192, 16384
+model=${7:-"llama3-8b-inst"}
+
+if [ "$model" = "llama3-8b-inst" ];then
+    model_path=meta-llama/Meta-Llama-3-8B-Instruct
+fi
+if [ "$model" = "llama3.1-8b-inst" ];then
+    model_path=meta-llama/Llama-3.1-8B-Instruct
+fi
+source_path=results/${model}/
+
 # merge_method=$7 # Support "pivot"(LOOK-M_PivotMerge).
 # quant_method=$7 # Support kivi and kvquant, default None.
 # nbits=$8 # Quantization bit-width support 8,4,2. Need to set quant_method first.
